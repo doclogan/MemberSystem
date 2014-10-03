dbd05d71846dc8b90814e2a4ed1b6c9432b8a55f
Derrick Logan <logan.derrick@gmail.com>
2014-10-03 02:00:39 -0500

    After initial  envelope editing

--- CBManager/MemberEditor/src/org/church/editor/EnvelopeEditorTopComponent.java
+++ CBManager/MemberEditor/src/org/church/editor/EnvelopeEditorTopComponent.java
@@ -5,11 +5,40 @@
  */
 package org.church.editor;
 
+import demo.Envelope;
+import demo.Member1;
+import demo.Memenv;
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import javax.persistence.TemporalType;
+import javax.persistence.TypedQuery;
+import javax.swing.Icon;
+import org.church.parameters.Parameters;
 import org.netbeans.api.settings.ConvertAsProperties;
+import org.netbeans.spi.actions.AbstractSavable;
+import org.openide.DialogDisplayer;
+import org.openide.NotifyDescriptor;
 import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
+import org.openide.awt.UndoRedo;
+import org.openide.util.ImageUtilities;
+import org.openide.util.Lookup;
+import org.openide.util.LookupEvent;
+import org.openide.util.LookupListener;
 import org.openide.windows.TopComponent;
 import org.openide.util.NbBundle.Messages;
+import org.openide.util.lookup.AbstractLookup;
+import org.openide.util.lookup.InstanceContent;
+import org.openide.windows.WindowManager;
 
 /**
  * Top component which displays something.
@@ -35,15 +64,149 @@ import org.openide.util.NbBundle.Messages;
     "CTL_EnvelopeEditorTopComponent=EnvelopeEditor Window",
     "HINT_EnvelopeEditorTopComponent=This is a EnvelopeEditor window"
 })
-public final class EnvelopeEditorTopComponent extends TopComponent {
-
+public final class EnvelopeEditorTopComponent extends TopComponent implements LookupListener{
+    private Lookup.Result result = null;
+    private UndoRedo.Manager manager = new UndoRedo.Manager();
+    InstanceContent ic = new InstanceContent();
+    Envelope envelope;
     public EnvelopeEditorTopComponent() {
         initComponents();
         setName(Bundle.CTL_EnvelopeEditorTopComponent());
         setToolTipText(Bundle.HINT_EnvelopeEditorTopComponent());
+        associateLookup(new AbstractLookup(ic));
+        total_field.getDocument().addUndoableEditListener(manager);
+        total_field.addKeyListener(new KeyAdapter() {
+        @Override
+        public void keyReleased(KeyEvent e) {
+            modify();
+        }
+        });
+    }
+    private void modify() {
+        if (getLookup().lookup(MySavable.class) == null) {
+            ic.add(new MySavable());
+        }
+    }
+    void resetFields() {
+        envelope = new Envelope();
+        total_field.setText("");
+    }
+    
+    private static final Icon ICON = ImageUtilities.loadImageIcon("org/shop/editor/Icon.png", true);
+    private class MySavable extends AbstractSavable implements Icon {
+
+        MySavable() {
+            register();
+        }
+
+        @Override
+        protected String findDisplayName() {
+            String fname = firstName_field.getText();
+            String lname = lastName_field.getText();
+            return fname + " " + lname;
+        }
+
+        @Override
+        protected void handleSave() throws IOException {
+            NotifyDescriptor.Confirmation message = new NotifyDescriptor.Confirmation("Do you want to save \""
+                    + firstName_field.getText() + " (" + lastName_field.getText() + ")\"?",
+                    NotifyDescriptor.OK_CANCEL_OPTION,
+                    NotifyDescriptor.QUESTION_MESSAGE);
+            Object result = DialogDisplayer.getDefault().notify(message);
+            //When user clicks "Yes", indicating they really want to save,
+            //we need to disable the Save action,
+            //so that it will only be usable when the next change is made
+            //to the JTextArea:
+            if (NotifyDescriptor.YES_OPTION.equals(result)) {
+                //Handle the save here...
+                EntityManager entityManager = Persistence.createEntityManagerFactory("MemberLibraryPU").createEntityManager();
+                entityManager.getTransaction().begin();
+                if (envelope.getIdenvelope() != null) {
+                    Envelope env = entityManager.find(Envelope.class, envelope.getIdenvelope());
+                    env.setTotal(new Integer(total_field.getText()));
+                    entityManager.getTransaction().commit();
+                    tc().ic.remove(this);
+                    unregister();
+                } else {
+                    Query query = entityManager.createNamedQuery("Envelope.findAll");
+                    List<Envelope> resultList = query.getResultList();
+                    envelope.setIdenvelope(resultList.size()+1);
+                    envelope.setTotal(new Integer(total_field.getText()));
+                    //add more fields that will populate all the other columns in the table!
+                    entityManager.persist(envelope);
+                    entityManager.getTransaction().commit();
+                }               
+            }
+        }
+        EnvelopeEditorTopComponent tc() {
+            return EnvelopeEditorTopComponent.this;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj instanceof MySavable) {
+                MySavable m = (MySavable) obj;
+                return tc() == m.tc();
+            }
+            return false;
+        }
+
+        @Override
+        public int hashCode() {
+            return tc().hashCode();
+        }
+
+        @Override
+        public void paintIcon(Component c, Graphics g, int x, int y) {
+            ICON.paintIcon(c, g, x, y);
+        }
+
+        @Override
+        public int getIconWidth() {
+            return ICON.getIconWidth();
+        }
+
+        @Override
+        public int getIconHeight() {
+            return ICON.getIconHeight();
+        }
 
     }
-
+    @Override
+    public void resultChanged(LookupEvent lookupEvent) {
+        Lookup.Result r = (Lookup.Result) lookupEvent.getSource();
+        Collection<Member1> coll = r.allInstances();
+        if (!coll.isEmpty()) {
+            for (Member1 mem : coll) {
+                Member1 member = mem;
+                firstName_field.setText(mem.getFirstName());
+                lastName_field.setText(mem.getLastName());
+                address1_field.setText(mem.getMemAddress1());
+                address2_field.setText(mem.getMemAddress2());
+                
+                envelope = getEnvelope(mem);
+                total_field.setText(envelope.getTotal().toString());
+            }
+        } else {
+            firstName_field.setText("[no name]");
+            lastName_field.setText("[no city]");
+            address1_field.setText("[no address1]");
+            address2_field.setText("[no address2]");
+        }
+    }
+    @Override
+    public UndoRedo getUndoRedo() {
+        return manager;
+    }
+    public Envelope getEnvelope(Member1 mem){
+        EntityManager em = Persistence.createEntityManagerFactory("MemberLibraryPU").createEntityManager();
+        TypedQuery<Memenv> tquery = em.createQuery(
+        "SELECT m FROM Memenv m WHERE m.envDate = :envDate and m.idmember = :idmember", Memenv.class);
+        Date date_param = Parameters.getInstance().getSabDate();
+        Memenv menv = tquery.setParameter("idmember", mem.getIdmember()).setParameter("envDate",date_param).getSingleResult();
+        Query query = em.createNamedQuery("Envelope.findByIdenvelope");
+        return (Envelope) query.setParameter("idenvelope", menv.getIdenvelope()).getSingleResult();
+    }
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -53,8 +216,8 @@ public final class EnvelopeEditorTopComponent extends TopComponent {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        firstName_Field = new javax.swing.JTextField();
-        lastNameField = new javax.swing.JTextField();
+        firstName_field = new javax.swing.JTextField();
+        lastName_field = new javax.swing.JTextField();
         address1_field = new javax.swing.JTextField();
         address2_field = new javax.swing.JTextField();
         jPanel2 = new javax.swing.JPanel();
@@ -65,9 +228,9 @@ public final class EnvelopeEditorTopComponent extends TopComponent {
         jScrollPane1 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
 
-        firstName_Field.setText(org.openide.util.NbBundle.getMessage(EnvelopeEditorTopComponent.class, "EnvelopeEditorTopComponent.firstName_Field.text")); // NOI18N
+        firstName_field.setText(org.openide.util.NbBundle.getMessage(EnvelopeEditorTopComponent.class, "EnvelopeEditorTopComponent.firstName_field.text")); // NOI18N
 
-        lastNameField.setText(org.openide.util.NbBundle.getMessage(EnvelopeEditorTopComponent.class, "EnvelopeEditorTopComponent.lastNameField.text")); // NOI18N
+        lastName_field.setText(org.openide.util.NbBundle.getMessage(EnvelopeEditorTopComponent.class, "EnvelopeEditorTopComponent.lastName_field.text")); // NOI18N
 
         address1_field.setText(org.openide.util.NbBundle.getMessage(EnvelopeEditorTopComponent.class, "EnvelopeEditorTopComponent.address1_field.text")); // NOI18N
 
@@ -81,9 +244,9 @@ public final class EnvelopeEditorTopComponent extends TopComponent {
                 .addContainerGap()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(firstName_Field, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(firstName_field, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(lastNameField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(lastName_field, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addComponent(address1_field, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(address2_field, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap(210, Short.MAX_VALUE))
@@ -93,8 +256,8 @@ public final class EnvelopeEditorTopComponent extends TopComponent {
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(firstName_Field, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(lastNameField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(firstName_field, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(lastName_field, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(address1_field, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -189,7 +352,7 @@ public final class EnvelopeEditorTopComponent extends TopComponent {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JTextField address1_field;
     private javax.swing.JTextField address2_field;
-    private javax.swing.JTextField firstName_Field;
+    private javax.swing.JTextField firstName_field;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
@@ -197,17 +360,22 @@ public final class EnvelopeEditorTopComponent extends TopComponent {
     private javax.swing.JPanel jPanel4;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTable1;
-    private javax.swing.JTextField lastNameField;
+    private javax.swing.JTextField lastName_field;
     private javax.swing.JTextField total_field;
     // End of variables declaration//GEN-END:variables
     @Override
     public void componentOpened() {
         // TODO add custom code on component opening
+        result = WindowManager.getDefault().findTopComponent("MemberViewerTopComponent").getLookup().lookupResult(Member1.class);
+        result.addLookupListener(this);
+        resultChanged(new LookupEvent(result));
     }
 
     @Override
     public void componentClosed() {
         // TODO add custom code on component closing
+        result.removeLookupListener(this);
+        result = null;
     }
 
     void writeProperties(java.util.Properties p) {
